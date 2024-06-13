564ad711d1726c3a46d871fc76f47ac2d207f0fa
vircantero <virgican2@gmail.com>
2024-06-13 19:24:18 -0300

    hola

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+document.getElementById('precioProducto').innerText = 'Precio $450';
+
+document.querySelector('#nombreProducto').style.color = 'blue';
+document.querySelector('#nombreProducto').style.fontSize = '24px';
